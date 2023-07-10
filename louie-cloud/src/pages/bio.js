f68bd487bfe455ce57d9777e97ad08a947768f64
leormston <l.e.ormston@gmail.com>
2023-07-11 00:09:47 +0100

    changed bio a bit and added my headshot

--- louie-cloud/src/pages/bio.js
+++ louie-cloud/src/pages/bio.js
@@ -1,11 +1,14 @@
 import * as React from 'react';
+import TechStack from '../components/techStack';
+import { createTheme } from '@mui/material/styles';
 
-
+const darkTheme = createTheme({ palette: { mode: 'dark' } });
 
 export default function Bio() {
     return (
         <div>
            <p>Bio</p>
+           <TechStack theme={darkTheme}/>
         </div>  
     )
 };
